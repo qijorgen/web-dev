7368baab251a5ca539b495a02223c8ff2618eeab
qijorgen <qijorgen@gmail.com>
2022-10-31 15:23:05 -0400

    react init

---
+++ calc-react/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+//scss
+function App() {
+  
+  let array = [1,2,3,4,5,6,7,8,9]
+
+  return (
+    <div className="App">
+      <div>
+        for loop here:
+        {
+          //for loop
+          
+        }
+      </div>
+      {
+        //todo: use map
+      }
+      {
+        //todo: use foreach
+      }
+    </div>
+  );
+}
+
+export default App;
