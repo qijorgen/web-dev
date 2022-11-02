4246eaf3d464758f38719b593901624cf41f37bf
qijorgen <qijorgen@gmail.com>
2022-11-02 16:53:51 -0400

    complete task 10/31

--- calc-react/src/App.js
+++ calc-react/src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 //scss
+//what's npm
+//what node
+//what's nvm
+//what's git, branch, remote? commit?
 function App() {
   
   let array = [1,2,3,4,5,6,7,8,9]
-
+  
+  function forLoop(array) {
+    const forarray = [];
+     for (let i=0; i< array.length; i++) {
+      forarray.push(<div>{array[i]}</div>) 
+    }
+    return <div>{forarray}</div>
+  }
+  function foreachLoop(array) {
+          
+    const foreachArray = []
+    array.forEach((ele) => {
+      foreachArray.push(<div>{ele}</div>)
+    })
+    return <div>{foreachArray}</div>
+  }
   return (
     <div className="App">
       <div>
         for loop here:
         {
-          //for loop
-          
+          forLoop(array)
         }
       </div>
+      <div>
+        map loop here:
       {
-        //todo: use map
+         array.map((ele,idx) => {
+          return <div key={idx}>{ele}</div>
+        })
       }
+      </div>
+      <div>
+        foreach loop here:
       {
         //todo: use foreach
+        foreachLoop(array)
       }
+      </div>
     </div>
   );
 }
