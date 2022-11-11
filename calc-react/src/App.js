7c33fa43176ef539f9a69aab6bac8f7c9a22307e
qijorgen <qijorgen@gmail.com>
2022-11-11 17:20:56 -0500

    user List state

--- calc-react/src/App.js
+++ calc-react/src/App.js
@@ -1,6 +1,8 @@
 import LoopListContainer from './Container/LoopListContainer'
 import Employee from './Component/Employee'
 import React from 'react'
+import axios from 'axios'
+
 import './App.css';
 import './App.scss';
 //scss/sass: super set of css
@@ -29,8 +31,10 @@ import './App.scss';
 //diff? why functional is better.
 function App() {
     const [empList, setEmpList] = React.useState([])
-    const [stringData, setStringData] = React.useState("")
-    const [numberData, setNumberData] = React.useState(99)
+    const [stringData, setStringData] = React.useState("default String")
+    const [numberData, setNumberData] = React.useState(1)
+    //list of users, on every api call, push a new user into the list, then render it
+    const [userList, setUserList] = React.useState([])
 
     //the first param
     //the second param: 
@@ -48,13 +52,32 @@ function App() {
         console.log(`...${stringData} has changed...`);
     }, [stringData])
 
+    //we will use get method, and use axios lib to help us make the api call
+    //
+    React.useEffect(() => {
+        console.log(numberData)
+        axios.get('https://jsonplaceholder.typicode.com/todos/' + numberData)//you don't get the api response result im
+            //but here, you can register a callback waiting
+            //whenever the result comes back, this CB will be invoked as a func
+            .then((resp) => {
+                console.log(resp.data);
+                //!!! userList.push(resp.data)
+                // ... spread/deconstruction
+                setUserList([...userList, resp.data])
+            });
+    }, [numberData])
+
+    
+
+
     //use the setter method, inside of useEffect. to generate random data, and render them.
 
     //employee data are being held in app(the parent component): state
     //employee data are passed down to child component as: props
     return (
         <div className="App">
-            stringData: { stringData}
+            <div>
+                stringData: { stringData}
             {
                 empList.map((anEmp, idx) => {
                     
@@ -64,6 +87,16 @@ function App() {
             <button onClick={() => {
                 setStringData(stringData + " _");
             }}>update String Data</button>
+            </div>
+            
+            <hr/>
+
+            <div>
+                {numberData}
+                <button onClick={() => {
+                    setNumberData(numberData + 1)
+                }}>update Number Data</button>
+            </div>
         </div>
     );
 }
